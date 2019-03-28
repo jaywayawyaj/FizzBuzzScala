083bcdcfdffe278fbe2df96c0630d6924064ac9a
Jethro Whaley <jethro.whaley@gmail.com>
2019-03-28 10:47:00 +0000

    Feat: returns buzz for multiples of 5

--- src/main/scala/FizzBuzz.scala
+++ src/main/scala/FizzBuzz.scala
@@ -2,6 +2,8 @@ object FizzBuzz extends App {
   def filter(num: Int) = {
     if (num % 3 == 0)
       "Fizz"
+    else if (num == 5)
+      "Buzz"
     else
       num
   }
--- src/test/scala/FizzBuzzTest.scala
+++ src/test/scala/FizzBuzzTest.scala
@@ -16,4 +16,8 @@ class FizzBuzzTest extends FunSuite {
   test("#filter returns fizz instead of multiples of 3 (30)") {
     assert(FizzBuzz.filter(33) === "Fizz")
   }
+
+  test("#filter returns fizz instead 5") {
+    assert(FizzBuzz.filter(5) === "Buzz")
+  }
 }
